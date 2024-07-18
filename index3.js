7d4804f84658b4f179cc6ef5b46584a0b3d491c1
David <david.fattal@leiainc.com>
2024-07-17 21:04:01 -0700

    Added Surprise !

---
+++ index3.js
@@ -0,0 +1,306 @@
+
+async function setupCamera() {
+  const video = document.getElementById('video');
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: true
+  });
+  video.srcObject = stream;
+
+  return new Promise((resolve) => {
+    video.onloadedmetadata = () => {
+      resolve(video);
+    };
+  });
+}
+
+function calculateAverageKeypoint(filteredKeypoints) {
+      if (filteredKeypoints.length === 0) {
+        return { x: 0, y: 0, z: 0 }; // or handle the empty case as needed
+      }
+      const sum = filteredKeypoints.reduce((acc, keypoint) => {
+        return {
+          x: acc.x + keypoint.x,
+          y: acc.y + keypoint.y,
+          z: acc.z + keypoint.z
+        };
+      }, { x: 0, y: 0, z: 0 });
+
+      return {
+        x: sum.x / filteredKeypoints.length,
+        y: sum.y / filteredKeypoints.length,
+        z: sum.z / filteredKeypoints.length
+      };
+    }
+
+function extractFacePosition(predictions) {
+    if (predictions.length > 0) {
+      const keypoints = predictions[0].keypoints;
+
+      // Calculate the center of the face
+      const leftEyePts = keypoints.filter(keypoint => keypoint.name && keypoint.name === "leftEye");
+      const rightEyePts = keypoints.filter(keypoint => keypoint.name && keypoint.name === "rightEye");
+      const leftEye = calculateAverageKeypoint(leftEyePts);
+      const rightEye = calculateAverageKeypoint(rightEyePts);
+
+      // Calculate distances (Assuming average interocular distance is 63mm)
+      const interocularDistance = Math.sqrt(
+        Math.pow(rightEye.x - leftEye.x, 2) +
+        Math.pow(rightEye.y - leftEye.y, 2) +
+        Math.pow(rightEye.z - leftEye.z, 2)
+      );
+      const focalLength = 640*1.0; // Focal length in pixels (estimated)
+      const realInterocularDistance = 63; // Real interocular distance in mm
+
+      const depth = (focalLength * realInterocularDistance) / interocularDistance;
+
+      const faceCenterX = (leftEye.x + rightEye.x) / 2;
+      const faceCenterY = (leftEye.y + rightEye.y) / 2;
+
+      // Convert face center to world coordinates
+      const x = -(faceCenterX - video.width / 2) * depth / focalLength;
+      const y = -(faceCenterY - video.height / 2) * depth / focalLength;
+      return {x:x, y:y, z:depth};
+    } else {
+      return {x:0, y:0, z:500};
+    }
+}
+
+// Function to load a shader from a file
+async function loadShaderFile(url) {
+  const response = await fetch(url + '?t=' + new Date().getTime()); // Append cache-busting query parameter);
+  return response.text();
+}
+
+async function loadImage(url) {
+  const img = new Image();
+  //img.src = url;
+  img.src = url + '?t=' + new Date().getTime(); // Append cache-busting query parameter
+  return new Promise((resolve) => {
+    img.onload = () => resolve(img);
+  });
+}
+
+function setupWebGL(gl, fragmentShaderSource) {
+  const vsSource = `
+    attribute vec4 aVertexPosition;
+    attribute vec2 aTextureCoord;
+    varying highp vec2 vTextureCoord;
+
+    void main(void) {
+      gl_Position = aVertexPosition;
+      vTextureCoord = aTextureCoord;
+    }
+  `;
+
+  const fsSource = fragmentShaderSource;
+
+  // Initialize shaders and program
+  const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
+  const programInfo = {
+    program: shaderProgram,
+    attribLocations: {
+      vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+      textureCoord: gl.getAttribLocation(shaderProgram, 'aTextureCoord'),
+    },
+    uniformLocations: {
+      uImage: gl.getUniformLocation(shaderProgram, 'uImage'),
+      uDisparityMap: gl.getUniformLocation(shaderProgram, 'uDisparityMap'),
+      uFacePosition: gl.getUniformLocation(shaderProgram, 'uFacePosition'),
+      iRes: gl.getUniformLocation(shaderProgram, 'iRes'),
+      oRes: gl.getUniformLocation(shaderProgram, 'oRes'),
+    },
+  };
+
+  // Vertex positions and texture coordinates
+  const positions = new Float32Array([
+    -1.0,  1.0,
+     1.0,  1.0,
+    -1.0, -1.0,
+     1.0, -1.0,
+  ]);
+  const textureCoords = new Float32Array([
+    0.0,  0.0,
+    1.0,  0.0,
+    0.0,  1.0,
+    1.0,  1.0,
+]);
+
+  const positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
+
+  const textureCoordBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, textureCoords, gl.STATIC_DRAW);
+
+  const indexBuffer = gl.createBuffer();
+  const indices = [0, 1, 2, 2, 1, 3];
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+
+  return { programInfo, buffers: { position: positionBuffer, textureCoord: textureCoordBuffer, indices: indexBuffer } };
+}
+
+function initShaderProgram(gl, vsSource, fsSource) {
+  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+
+  const shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    console.error('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+    return null;
+  }
+
+  return shaderProgram;
+}
+
+function loadShader(gl, type, source) {
+  const shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    console.error('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+    return null;
+  }
+
+  return shader;
+}
+
+function createTexture(gl, image) {
+  const texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+
+  return texture;
+}
+
+function drawScene(gl, programInfo, buffers, textures, facePosition) {
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  gl.useProgram(programInfo.program);
+
+  // Vertex positions
+  {
+    const numComponents = 2;
+    const type = gl.FLOAT;
+    const normalize = false;
+    const stride = 0;
+    const offset = 0;
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+    gl.vertexAttribPointer(programInfo.attribLocations.vertexPosition, numComponents, type, normalize, stride, offset);
+    gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
+  }
+
+  // Texture coordinates
+  {
+    const numComponents = 2;
+    const type = gl.FLOAT;
+    const normalize = false;
+    const stride = 0;
+    const offset = 0;
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.textureCoord);
+    gl.vertexAttribPointer(programInfo.attribLocations.textureCoord, numComponents, type, normalize, stride, offset);
+    gl.enableVertexAttribArray(programInfo.attribLocations.textureCoord);
+  }
+
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
+
+  gl.activeTexture(gl.TEXTURE0);
+  gl.bindTexture(gl.TEXTURE_2D, textures.albedo);
+  gl.uniform1i(programInfo.uniformLocations.uImage, 0);
+
+  gl.activeTexture(gl.TEXTURE1);
+  gl.bindTexture(gl.TEXTURE_2D, textures.disparity);
+  gl.uniform1i(programInfo.uniformLocations.uDisparityMap, 1);
+  gl.uniform3f(programInfo.uniformLocations.uFacePosition, facePosition.x, facePosition.y, facePosition.z);
+  //gl.uniform2f(programInfo.uniformLocations.iRes, albedoImage.width, albedoImage.height);  // Add this line
+  gl.uniform2f(programInfo.uniformLocations.iRes, textures.width, textures.height);  // Add this line
+  gl.uniform2f(programInfo.uniformLocations.oRes, gl.canvas.width, gl.canvas.height);      // Add this line
+
+  const vertexCount = 6;
+  const type = gl.UNSIGNED_SHORT;
+  const offset = 0;
+  gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
+}
+
+async function getFacePosition() {
+  const facePosition = await estimatePose();
+  console.log(facePosition);
+  return facePosition;
+}
+
+async function main() {
+
+  const video = await setupCamera();
+  video.play();
+
+  let facePosition = {x: 0, y: 0, z: 630};
+  let oldFacePosition = {x: 0, y: 0, z: 630};
+  const axy = 0.5; // exponential smoothing
+  const az = 0.1; // exponential smoothing
+
+  const model = faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh;
+  const detectorConfig = {
+    runtime: 'tfjs',
+  };
+  const detector = await faceLandmarksDetection.createDetector(model, detectorConfig);
+  const canvas = document.getElementById('glCanvas');
+  const gl = canvas.getContext('webgl');
+
+  if (!gl) {
+    console.error('Unable to initialize WebGL. Your browser or machine may not support it.');
+    return;
+  }
+
+  //const fragmentShaderSource = await loadShaderFile('./fragmentShader.glsl');
+  const fragmentShaderSource = await loadShaderFile('./rayCast.glsl');
+  const albedoImage = await loadImage('./images/albedo.jpg');
+  const disparityImage = await loadImage('./images/disparity.png');
+
+  // Set canvas size to match image aspect ratio
+  canvas.width = albedoImage.width;
+  canvas.height = albedoImage.height;
+
+  const textures = {
+    albedo: createTexture(gl, albedoImage),
+    disparity: createTexture(gl, disparityImage),
+    width: albedoImage.width,
+    height: albedoImage.height
+  };
+
+  const { programInfo, buffers } = setupWebGL(gl, fragmentShaderSource);
+
+  async function render() {
+    const estimationConfig = {flipHorizontal: false};
+    const predictions = await detector.estimateFaces(video, estimationConfig);
+    const newFacePosition = extractFacePosition(predictions);
+    facePosition.x = (1-axy)*oldFacePosition.x + axy*newFacePosition.x;
+    facePosition.y = (1-axy)*oldFacePosition.y + axy*newFacePosition.y;
+    facePosition.z = (1-az)*oldFacePosition.z + az*newFacePosition.z;
+    oldFacePosition = facePosition;
+
+    drawScene(gl, programInfo, buffers, textures, facePosition);
+    requestAnimationFrame(render);
+    console.log(facePosition);
+  }
+
+  render();
+}
+
+main();
+
+
---
+++ rayCast.glsl
@@ -0,0 +1,193 @@
+precision highp float;
+varying highp vec2 vTextureCoord;
+uniform sampler2D uImage;
+uniform sampler2D uDisparityMap;
+uniform vec3 uFacePosition;
+uniform vec2 iRes, oRes;
+
+/*vec4 texture2(sampler2D iChannel, vec2 coord) {
+    ivec2 ivec = ivec2(int(coord.x * iRes.x),  // asssuming all input textures are of same size
+                       int(coord.y * iRes.y));
+    return texelFetch(iChannel, ivec, 0);
+}*/
+#define texture texture2D
+
+const float vd = 600.0;
+const float IO = 63.0;
+const float f = 1.0;
+
+vec2 adjustAr(vec2 iRes,vec2 oRes) {
+    float s = min(iRes.x,iRes.y)/min(oRes.x,oRes.y);
+    return s*oRes/iRes;
+}
+
+vec3 readColor(sampler2D iChannel, vec2 uv) { return texture(iChannel, uv).rgb; }
+float readDisp(sampler2D iChannel, vec2 uv, float minDisp, float maxDisp) { return texture(iChannel, vec2(clamp(uv.x,2.0/iRes.x,1.0-2.0/iRes.x),clamp(uv.y,2.0/iRes.y,1.0-2.0/iRes.y))).x * (minDisp - maxDisp) + maxDisp; }
+
+float disp2Color(float disp, float minDisp, float maxDisp) { return (disp-maxDisp)/(minDisp-maxDisp); }
+float color2Disp(float col, float minDisp, float maxDisp) { return col * (minDisp - maxDisp) + maxDisp; }
+float disp2invZ(float disp) { return -disp/f; } // returns 1/z
+float invZ2Disp(float invZ) { return -f*invZ; } // returns disp
+
+mat3 matFromSlant(vec2 sl) {
+
+    // builds rotation matrix from slant (tangent space) info
+    float invsqx = 1.0/sqrt(1.0+sl.x*sl.x);
+    float invsqy = 1.0/sqrt(1.0+sl.y*sl.y);
+    float invsq = 1.0/sqrt(1.0+sl.x*sl.x+sl.y*sl.y);
+
+    return mat3(invsqx,0.0,sl.x*invsq,0.0,invsqy,sl.y*invsq,-sl.x*invsqx,-sl.y*invsqy,invsq);
+}
+
+mat3 matFromRoll(float th) {
+
+    // builds rotation matrix from roll angle
+    float PI = 3.141593;
+    float c = cos(th*PI/180.0);
+    float s = sin(th*PI/180.0);
+
+    return mat3(c,s,0.0,-s,c,0.0,0.0,0.0,1.0);
+}
+
+mat3 matFromSkew(vec2 sk) {
+
+    // builds frustum skew matrix from tangent angles
+    return mat3(1.0,0.0,0.0,0.0,1.0,0.0,-sk.x,-sk.y,1.0);
+
+}
+
+mat3 matFromFocal(vec2 fxy) {
+
+    // builds focal matrix
+    // includes correction for aspect ratio since f expressed in fraction image width
+    return mat3(fxy.x,0.0,0.0,0.0,fxy.y,0.0,0.0,0.0,1.0);
+}
+
+// Matrix Math
+float det(mat2 matrix) {
+    return matrix[0].x * matrix[1].y - matrix[0].y * matrix[1].x;
+}
+
+mat3 transpose_m(mat3 matrix){
+    return mat3(
+    vec3(matrix[0].x, matrix[1].x, matrix[2].x),
+    vec3(matrix[0].y, matrix[1].y, matrix[2].y),
+    vec3(matrix[0].z, matrix[1].z, matrix[2].z));
+}
+
+mat3 inverse(mat3 matrix) {
+    vec3 row0 = matrix[0];
+    vec3 row1 = matrix[1];
+    vec3 row2 = matrix[2];
+    vec3 minors0 = vec3(
+    det(mat2(row1.y, row1.z, row2.y, row2.z)),
+    det(mat2(row1.z, row1.x, row2.z, row2.x)),
+    det(mat2(row1.x, row1.y, row2.x, row2.y))
+    );
+    vec3 minors1 = vec3(
+    det(mat2(row2.y, row2.z, row0.y, row0.z)),
+    det(mat2(row2.z, row2.x, row0.z, row0.x)),
+    det(mat2(row2.x, row2.y, row0.x, row0.y))
+    );
+    vec3 minors2 = vec3(
+    det(mat2(row0.y, row0.z, row1.y, row1.z)),
+    det(mat2(row0.z, row0.x, row1.z, row1.x)),
+    det(mat2(row0.x, row0.y, row1.x, row1.y))
+    );
+    mat3 adj = transpose_m(mat3(minors0, minors1, minors2));
+    return (1.0 / dot(row0, minors0)) * adj;
+}
+
+// Action !
+
+vec4 raycasting(vec2 s2, mat3 FSKR2, vec3 C2, mat3 FSKR1, vec3 C1, sampler2D iChannelCol, sampler2D iChannelDisp, float minDisp, float maxDisp, float t) {
+
+    // s2 is normalized xy coordinate for synthesized view, centered at 0 so values in -0.5..0.5
+
+    const int numsteps = 40;
+    float numsteps_float = float(numsteps);
+
+    float invZ = disp2invZ(minDisp); // starting point for invZ search
+    float dinvZ = (invZ - disp2invZ(maxDisp)) / numsteps_float; // dividing the step into 40 steps
+    float invZmin = invZ*(1.0-t); // for animation
+    invZ += dinvZ; // step back once before start
+
+    //vec2 s1 = s2; // inititalize s1
+    float invZ2 = 0.0; // initialize invZ2
+    float disp = 0.0; //initialize disp
+    float oldDisp = 0.0;
+    float gradDisp = 0.0;
+    float gradThr = 0.02*(maxDisp-minDisp)*140.0/numsteps_float;
+
+    vec3 nor = vec3(0.0);
+
+    mat3 P = FSKR1*inverse(FSKR2);
+    vec3 C = FSKR1*(C2-C1);
+
+    // extract matrix blocks
+    mat2 Pxyxy = mat2(P[0].xy, P[1].xy);
+    vec2 Pxyz = P[2].xy;
+    vec2 Pzxy = vec2(P[0].z, P[1].z);
+    float Pzz = P[2].z;
+
+    vec2 s1 = C.xy*invZ + (1.0 - C.z*invZ)*(Pxyxy*s2 + Pxyz)/(dot(Pzxy,s2) + Pzz); // starting point for s1
+    vec2 ds1 = (C.xy-C.z*(Pxyxy*s2 + Pxyz)/(dot(Pzxy,s2) + Pzz))*dinvZ; // initial s1 step size
+
+    float alpha = 1.0;
+    // 40 steps
+    for (int i = 0; i < numsteps; i++) {
+
+        invZ -= dinvZ; // step forward
+        s1 -= ds1;
+
+        //s1 = C.xy*invZ + (1.0 - C.z*invZ)*(Pxyxy*s2 + Pxyz)/(dot(Pzxy,s2) + Pzz);
+
+        disp = readDisp(iChannelDisp, s1 + .5,minDisp,maxDisp);
+        gradDisp = disp-oldDisp;
+        oldDisp = disp;
+        invZ2 = invZ*(dot(Pzxy,s2) + Pzz)/(1.0 - C.z*invZ);
+        if ((disp < invZ2Disp(invZ))&&(invZ2>0.0)) { // if ray is below the "virtual surface"...
+            //if (invZ2Disp(invZ)-disp > 0.002) alpha = 0.0;
+            if (abs(gradDisp) > gradThr) alpha = 0.0;
+            invZ += dinvZ; // step back
+            s1 += ds1;
+            dinvZ /= 2.0; // increase precision
+            ds1 /= 2.0;
+        }
+
+    }
+    if ((abs(s1.x)<0.5)&&(abs(s1.y)<0.5)&&(invZ2>0.0)&&(invZ>invZmin)) {
+    //if ((abs(s1.x*adjustAr(iChannelResolution[0].xy,iResolution.xy).x)<0.495)&&(abs(s1.y*adjustAr(iChannelResolution[0].xy,iResolution.xy).y)<0.495)&&(invZ2>0.0)) {
+        return vec4(readColor(iChannelCol, s1+.5), alpha*invZ2);
+    } else {
+        return vec4(0.0);
+    }
+}
+
+void main(void) {
+
+    vec2 uv = vTextureCoord;
+
+    float minDisp = -IO*f/vd;
+    float maxDisp = minDisp/2.0;
+    float invZmin = disp2invZ(minDisp);
+    float invZmax = disp2invZ(maxDisp);
+
+    vec2 f1 = vec2(f,f*iRes.x/iRes.y);
+    mat3 FSKR1 = mat3(f1.x,0.0,0.0,0.0,f1.y,0.0,0.0,0.0,1.0); // only f matrix is on trivial, no frustum skew no rotation
+    vec3 C1 = vec3(0.0,0.0,-1.0/invZmin); // original position
+
+    vec3 C2 = vec3(uFacePosition.x,-uFacePosition.y,-uFacePosition.z)/IO; // normalized camera space coordinates
+    vec2 sk2 = C2.xy/C2.z; //keeps 3D focus at specified location
+    vec2 f2 = f1/adjustAr(iRes,oRes)*C2.z/C1.z; // extra numerical zoom for exit effect only
+    //vec2 f2 = f1/adjustAr(iRes,oRes);
+
+    mat3 FSKR2 = matFromFocal(f2)*matFromSkew(sk2); // non need for extra rot calculation here
+
+    vec4 result = raycasting(uv-0.5, FSKR2, C2, FSKR1, C1, uImage, uDisparityMap, minDisp, maxDisp, 1.0);
+    gl_FragColor = vec4(result.rgb,1.0);
+
+    //vec4 albedoColor = texture2D(uImage, vTextureCoord);
+    //float disparity = texture2D(uDisparityMap, vTextureCoord).r;
+    //gl_FragColor = vec4(albedoColor.rgb,1.0);
+}
