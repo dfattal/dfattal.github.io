5d896ee4578c887a1f5b9ebd112233bf19f18b46
David <david.fattal@leiainc.com>
2024-07-24 14:43:07 +0800

    new testVideo

---
+++ testVideo.js
@@ -0,0 +1,47 @@
+
+
+async function main() {
+    try {
+        // Request the user-facing camera
+        const constraints = {
+            video: {
+                facingMode: { ideal: 'user' }       // to get the best FOV
+            }
+        };
+
+        // Get the media stream
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+
+        // Create a video element to play the stream
+        const video = document.createElement('video');
+        video.srcObject = stream;
+        video.onloadedmetadata = () => {
+            console.log(`Actual video resolution: ${video.videoWidth}x${video.videoHeight}`);
+            document.getElementById("resolution").textContent = [video.videoWidth,video.videoHeight];
+        };
+        document.body.appendChild(video);
+
+        iOSmsg = document.getElementById("iOSmsg");
+        function startVideo() {
+            iOSmsg.remove();
+            video.play();
+        }
+
+        if (isIOS()) {
+          console.log("iOS Device Detected");
+          iOSmsg.textContent = "iOS Device Detected. Click to start video.";
+          document.addEventListener('click', startVideo, { once: true });
+        } else {
+          startVideo();
+        }
+
+
+
+    } catch (error) {
+        console.error('Error accessing user-facing camera:', error);
+    }
+}
+
+// Call the function to get the camera feed
+main();
+
