0ada7b13560db5f54ac6e127758079ccf7bdbb21
David <david.fattal@leiainc.com>
2024-10-24 18:08:22 -0700

    try again device rotation

--- LIF/LIF.js
+++ LIF/LIF.js
@@ -1116,16 +1116,16 @@ class lifViewer {
                 // console.log(`(${relativeX}, ${relativeY}`); // Outputs values between -0.5 and +0.5
             }.bind(this));
             // Device tilt listener for mobile devices
-            // window.addEventListener('deviceorientation', function (event) {
-            //     // event.beta is the tilt in the x-axis (front-back tilting), normalized between -90 and +90
-            //     // event.gamma is the tilt in the y-axis (left-right tilting), normalized between -90 and +90
+            window.addEventListener('deviceorientation', function (event) {
+                // event.beta is the tilt in the x-axis (front-back tilting), normalized between -90 and +90
+                // event.gamma is the tilt in the y-axis (left-right tilting), normalized between -90 and +90
 
-            //     // Normalize beta and gamma to a value between -0.5 and +0.5
-            //     this.mousePos.y = event.beta / 45;  // Normalized value for front-back tilt
-            //     this.mousePos.x = event.gamma / 45;  // Normalized value for left-right tilt
+                // Normalize beta and gamma to a value between -0.5 and +0.5
+                this.mousePos.y = event.beta / 45;  // Normalized value for front-back tilt
+                this.mousePos.x = event.gamma / 45;  // Normalized value for left-right tilt
 
-            //     // console.log(`Tilt X: ${this.tilt.x}, Tilt Y: ${this.tilt.y}`); // Log the tilt values
-            // });
+                // console.log(`Tilt X: ${this.tilt.x}, Tilt Y: ${this.tilt.y}`); // Log the tilt values
+            });
             this.gl = this.canvas.getContext('webgl');
             await this.initWebGLResources();
             this.startAnimation();
